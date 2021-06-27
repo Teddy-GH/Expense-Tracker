a09cdb0b5451b7f9e6cf0a9fdf36ad86641736f0
TeddyGirma20 <teddygirma20@gmail.com>
2021-06-27 22:09:38 +0300

    Expense-UI-completion

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
-import {Header} from './components/Header';
-import {Balance} from './components/Balance';
-import {IncomeExpenses} from './components/IncomeExpenses';
-import {TransactionList} from './components/TransactionList';
-import {AddTransaction} from './components/AddTransaction';
-
-import {GlobalProvider} from './context/GlobalState';
-
-
-
 
 function App() {
   return (
     <GlobalProvider>
       <Header />
-    <div className="container">
-         <Balance />
-         < IncomeExpenses />
-         <TransactionList />
-         < AddTransaction />
-    </div>
-
-
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </GlobalProvider>
   );
 }
